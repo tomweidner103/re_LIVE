30652875419a6302c26e3e84dd884d613002d4d9
tomweidner103 <tomweidner103@gmail.com>
2019-10-15 13:45:00 -0700

    Delete msg store

--- client/reducers/messages.js
+++ client/reducers/messages.js
@@ -2,6 +2,7 @@ import axios from "axios"
 
 const GET_MESSAGES = 'GET_MESSAGES'
 const POST_MESSAGE = 'POST_MESSAGE'
+const DELETE_MESSAGE = "DELETE_MESSAGE'
 
 const getMessages = (messages) => ({
     type: GET_MESSAGES,
@@ -11,6 +12,11 @@ const getMessages = (messages) => ({
 const postMessage = (message) => ({
     type: POST_MESSAGE,
     message
+})
+
+const deleteMessage = (id) => ({
+    type: DELETE_MESSAGE,
+    id
 })
 
 export const getMessageThunk = () => async dispatch => {
@@ -23,12 +29,19 @@ export const postMessageThunk =(body) => async dispatch => {
     dispatch(postMessage(data))
 }
 
+export const deleteMessageThunk = (id) => async dispatch => {
+    await axios.delete(`/api/messages/${id}`)
+    dispatch(deleteMessage(id))
+}
+
 const messageReducer = (state=[], action) => {
     switch(action.type){
         case GET_MESSAGES:
             return action.messages
         case POST_MESSAGE:
-            return [action.message, ...state]
+            return [...state, action.message]
+        case DELETE_MESSAGE:
+            return [...state].filter(msg => msg.id !== action.id)
         default:
             return state
     }
